80a3beaf4d38d3dfa314f935afd101f305300fcb
Benjamin Rannow <b.rannow@familie-redlich.de>
2020-10-05 10:49:42 +0200

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'BR ToolKit',
+    'description' => 'Generic Quality of Life Library of Modules/Code',
+    'category' => 'misc',
+    'author' => 'Benjamin Rannow',
+    'author_email' => 'b.rannow@familie-redlich.de',
+    'author_company' => 'Familie-Redlich-Digital GmbH',
+    'state' => 'alpha',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => [
+      'depends' => [
+        'typo3' => '9.5.0-9.5.99'
+      ],
+      'conflicts' => [],
+      'suggests' => []
+    ]
+];
+
