aa0998a3ad5465ad56ef6a7fb16bd6e10e869de8
DDEV-Local User <nobody@example.com>
2020-10-05 14:57:45 +0200

    add first TestCase (example)

---
+++ Tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BR\Toolkit\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExampleMethod()
+    {
+        $this->assertTrue(true);
+    }
+}
