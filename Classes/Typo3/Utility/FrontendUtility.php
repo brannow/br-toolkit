d79d47c9e69c9a020ff7f3d809c0a8a5fb96c7a4
Benjamin Rannow <b.rannow@familie-redlich.de>
2021-06-10 08:15:02 +0200

    add frontend uribuilder creation method

--- Classes/Typo3/Utility/FrontendUtility.php
+++ Classes/Typo3/Utility/FrontendUtility.php
@@ -13,20 +13,52 @@ use TYPO3\CMS\Core\Routing\PageArguments;
 use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
+use TYPO3\CMS\Extbase\Service\EnvironmentService;
+use TYPO3\CMS\Extbase\Service\ExtensionService;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 abstract class FrontendUtility
 {
     /**
-     * @param Site|null $site
-     * @param SiteLanguage|null $siteLanguage
+     * @var UriBuilder|null
+     */
+    protected static ?UriBuilder $uriBuilder = null;
+
+    /**
+     * @param int $pageId
+     * @param int $langId
+     * @param int $type
      * @return TypoScriptFrontendController|null
      * @throws Typo3ConfigException
      * @throws \TYPO3\CMS\Core\Error\Http\InternalServerErrorException
      * @throws \TYPO3\CMS\Core\Error\Http\ServiceUnavailableException
      */
-    public static function getFrontendController(?Site $site = null, ?SiteLanguage $siteLanguage = null): ?TypoScriptFrontendController
+    public static function getFrontendControllerWithPageId(int $pageId, int $langId = 0, int $type = 0): ?TypoScriptFrontendController
+    {
+        $site = static::getSite($pageId);
+        if ($langId > 0) {
+            $lang = $site->getLanguageById($langId);
+        } else {
+            $lang = $site->getDefaultLanguage();
+        }
+
+        return static::getFrontendController($site, $lang, $type);
+    }
+
+    /**
+     * @param Site|null $site
+     * @param SiteLanguage|null $siteLanguage
+     * @param int $type
+     * @return TypoScriptFrontendController|null
+     * @throws Typo3ConfigException
+     * @throws \TYPO3\CMS\Core\Error\Http\InternalServerErrorException
+     * @throws \TYPO3\CMS\Core\Error\Http\ServiceUnavailableException
+     */
+    public static function getFrontendController(?Site $site = null, ?SiteLanguage $siteLanguage = null, int $type = 0): ?TypoScriptFrontendController
     {
         if ($GLOBALS['TSFE'] instanceof TypoScriptFrontendController) {
             // only use the current existing tsfe if the site lang is correct
@@ -45,7 +77,7 @@ abstract class FrontendUtility
             $siteLanguage =  $site->getDefaultLanguage();
         }
 
-        $pageArguments = InstanceUtility::get(PageArguments::class, $site->getRootPageId(), 0, []);
+        $pageArguments = InstanceUtility::get(PageArguments::class, $site->getRootPageId(), $type, []);
         /** @var NullFrontend $nullFrontend */
         $nullFrontend = InstanceUtility::get(NullFrontend::class, 'pages');
         $cacheManager = InstanceUtility::get(CacheManager::class);
@@ -96,5 +128,39 @@ abstract class FrontendUtility
         } else {
             throw new Typo3ConfigException('missing site config');
         }
+    }
+
+    /**
+     * @param Site|null $site
+     * @param SiteLanguage|null $siteLanguage
+     * @param int $type
+     * @return UriBuilder
+     * @throws Typo3ConfigException
+     * @throws \TYPO3\CMS\Core\Error\Http\InternalServerErrorException
+     * @throws \TYPO3\CMS\Core\Error\Http\ServiceUnavailableException
+     */
+    public static function getUriBuilder(?Site $site = null, ?SiteLanguage $siteLanguage = null, int $type = 0): UriBuilder
+    {
+        if (static::$uriBuilder === null) {
+            static::getFrontendController($site, $siteLanguage, $type);
+            /** @var UriBuilder $uriBuilder */
+            $uriBuilder = InstanceUtility::get(UriBuilder::class);
+            /** @var ContentObjectRenderer $contentObjectRenderer */
+            $contentObjectRenderer = InstanceUtility::get(ContentObjectRenderer::class);
+            /** @var ConfigurationManager $configurationManager */
+            $configurationManager = InstanceUtility::get(ConfigurationManager::class);
+            /** @var EnvironmentService $envService */
+            $envService = InstanceUtility::get(EnvironmentService::class);
+            /** @var ExtensionService $extService */
+            $extService = InstanceUtility::get(ExtensionService::class);
+            $configurationManager->setContentObject($contentObjectRenderer);
+            $uriBuilder->injectEnvironmentService($envService);
+            $uriBuilder->injectConfigurationManager($configurationManager);
+            $uriBuilder->injectExtensionService($extService);
+            $uriBuilder->initializeObject();
+            static::$uriBuilder = $uriBuilder;
+        }
+
+        return static::$uriBuilder->reset();
     }
 }
