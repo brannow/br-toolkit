e88e5aea5c2e942cc7d8c08670b0bf6aa2710484
Benjamin Rannow <b.rannow@familie-redlich.de>
2023-05-15 13:02:07 +0200

    add new PersistedEscapedAliasMapper

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,5 +5,7 @@ if (!defined('TYPO3_MODE')) {
 call_user_func(function ($extKey) {
 
     \BR\Toolkit\Typo3\Cache\CacheManager::announceCache();
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['PersistedEscapedAliasMapper'] =
+        \BR\Toolkit\Typo3\Routing\Aspect\PersistedEscapedAliasMapper::class;
 
 }, 'br_toolkit');
